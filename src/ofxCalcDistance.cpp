a849e2e7ade43b0ff516b9a7883c0955c6613ce1
okuyama <yutaokuyam@gmail.com>
2020-02-07 03:59:53 +0900

    add grouping function and linearFunc

--- src/ofxCalcDistance.cpp
+++ src/ofxCalcDistance.cpp
@@ -24,3 +24,18 @@ float  ofxCalcDistance::line2point(vec3 pos0,vec3 pos1,vec3 dstPos){
     
 }
 
+
+int ofxCalcDistance::grouping_2D(vec3 pos0,vec3 pos1,vec3 targetPos){
+    if(targetPos.y>=linearFunc(targetPos.x,pos0,pos1)){
+        return 1;
+    }else{
+        return 0;
+    }
+    return -1;
+}
+
+
+float ofxCalcDistance::linearFunc(float x,vec3 pos0,vec3 pos1){
+    return ((pos1.y-pos0.y)/(pos1.x-pos0.x))*(x-pos0.x)+pos0.y;
+    //2点間を結ぶ直線の関数値を計算
+}
--- src/ofxCalcDistance.h
+++ src/ofxCalcDistance.h
@@ -11,6 +11,8 @@ using namespace glm;
 class ofxCalcDistance{
 public:
     float line2point(vec3 pos0,vec3 pos1,vec3 dstPos);//2D
+    int grouping_2D(vec3 pos0,vec3 pos1,vec3 targetPos); //true above false below
+    float linearFunc(float x,vec3 pos0,vec3 pos1);
     
 };
 
